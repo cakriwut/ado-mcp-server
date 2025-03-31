2e42c0c18dc2ee4b5424fa2a569ddb4bd1bbe2fd
Riwut Libinuko <riwut.libinuko@sela.com.sg>
2025-03-31 17:40:17 +0800

    fix: incorrect handling /r /t

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -15,10 +15,10 @@ export function processEscapeSequences(input: string): string {
   // Replace common escape sequences with their actual characters
   return input
     .replace(/\\n/g, '\n')   // newline
-    .replace(/\\t/g, '\t')   // tab
-    .replace(/\\r/g, '\r')   // carriage return
-    .replace(/\\b/g, '\b')   // backspace
-    .replace(/\\f/g, '\f')   // form feed
-    .replace(/\\v/g, '\v')   // vertical tab
-    .replace(/\\\\/g, '\\'); // backslash
+    // .replace(/\\t/g, '\t')   // tab
+    // .replace(/\\r/g, '\r')   // carriage return
+    // .replace(/\\b/g, '\b')   // backspace
+    // .replace(/\\f/g, '\f')   // form feed
+    // .replace(/\\v/g, '\v')   // vertical tab
+    // .replace(/\\\\/g, '\\'); // backslash
 }
